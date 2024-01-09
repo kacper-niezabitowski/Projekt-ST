065f751845784228274223f7e25169d6b0d84bff
Agnieszka Żywiec <aga.zywiec@gmail.com>
2024-01-09 14:12:57 +0100

    Tryb ciemny logowanie + rejestracja

--- wyniki_na_zywo/src/components/LoginForm.js
+++ wyniki_na_zywo/src/components/LoginForm.js
@@ -3,10 +3,12 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import Navbar from './Navbar'; // Import the Navbar component
 import './LoginForm.css'; // Add styling for the login form
+import { useTheme } from './ThemeContext';
 
 const LoginForm = ({ onLogin, onRegister }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const { isDarkMode } = useTheme();
 
   const handleLogin = () => {
     // Authentication logic can be added here
@@ -21,36 +23,38 @@ const LoginForm = ({ onLogin, onRegister }) => {
   };
 
   return (
-    <div>
-      {/* Render the Navbar component */}
-      <Navbar />
+    <div className={isDarkMode ? 'dark-mode' : ''}>
+      <div>
+        {/* Render the Navbar component */}
+        <Navbar />
 
-      {/* Render the login form */}
-      <div className="login-form">
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
+        {/* Render the login form */}
+        <div className="login-form">
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
 
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-        <div>
-          <button onClick={handleLogin}>Login</button>
-        </div>
-        <div>
-          {/* Use Link to navigate to the /register page */}
-          <Link to="/register">
-            <button onClick={handleRegister}>Register</button>
-          </Link>
+          <div>
+            <button onClick={handleLogin}>Login</button>
+          </div>
+          <div>
+            {/* Use Link to navigate to the /register page */}
+            <Link to="/register">
+              <button onClick={handleRegister}>Register</button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
--- wyniki_na_zywo/src/components/RegisterForm.js
+++ wyniki_na_zywo/src/components/RegisterForm.js
@@ -3,12 +3,14 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from './Navbar';
 import './RegisterForm.css'; // Dodaj stylowanie dla formularza rejestracji
+import { useTheme } from './ThemeContext';
 
 const RegisterForm = ({ onRegister }) => {
   const [email, setEmail] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [repeatPassword, setRepeatPassword] = useState('');
+  const { isDarkMode } = useTheme();
 
   const handleRegister = () => {
     if (password === repeatPassword) {
@@ -23,46 +25,48 @@ const RegisterForm = ({ onRegister }) => {
   };
 
   return (
-    <div>
-      <Navbar />
-      <div className="login-form">
-      <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-              
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
+    <div className={isDarkMode ? 'dark-mode' : ''}>
+      <div>
+        <Navbar />
+        <div className="login-form">
+        <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+                
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
 
-        <label htmlFor="repeatPassword">Repeat Password:</label>
-        <input
-          type="password"
-          id="repeatPassword"
-          value={repeatPassword}
-          onChange={(e) => setRepeatPassword(e.target.value)}
-        />
+          <label htmlFor="repeatPassword">Repeat Password:</label>
+          <input
+            type="password"
+            id="repeatPassword"
+            value={repeatPassword}
+            onChange={(e) => setRepeatPassword(e.target.value)}
+          />
 
-        <div>
-          <button onClick={handleRegister}>Register</button>
-        </div>
-        <div>
-          <Link to="/login">Already have an account? Login here</Link>
+          <div>
+            <button onClick={handleRegister}>Register</button>
+          </div>
+          <div>
+            <Link to="/login">Already have an account? Login here</Link>
+          </div>
         </div>
       </div>
     </div>
